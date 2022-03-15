58ac6bb9d7c81853c91d80567a7b08928f829b64
Bluewind <bluewind@kakao.com>
2022-03-16 00:55:05 +0900

    페리카나 크롤링 추가

--- src/main/java/com/mukcha/controller/CrawlingController.java
+++ src/main/java/com/mukcha/controller/CrawlingController.java
@@ -10,6 +10,7 @@ import com.mukcha.service.FoodService;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -28,36 +29,73 @@ public class CrawlingController {
 
 
     @GetMapping(value = "/crawling")
-    public String crawling() throws IOException {
-        String getFoodName;
-        String getImageUrl;
-        String url = "http://www.chickenplus.co.kr/menu/default.aspx?menu=치킨메뉴";
-
-        Document doc = Jsoup.connect(url).get();
-
-        Elements lists = doc.select("div[class = mn_area]").select("div[class = mn1]");
-
-        Elements foodName = doc.select("div[class=mn1_txt]").select("div[class=mn1_tit menu_s_title]");
-        Elements image = doc.select("div[class=mn1_pd]").select("img[class=thumbMenu]");
-
-        System.out.println(">>> "+lists+" / size: "+lists.size());
-        for (int i=0; i<lists.size(); i++) {
-            // menu name
-            getFoodName = foodName.get(i).text();
-            System.out.println(">>> food name: "+getFoodName);
-            // image URL
-            getImageUrl = image.get(i).absUrl("src");
-            System.out.println(">>> url: "+getImageUrl);
-            // 만약 같은 이름의 메뉴가 없다면 DB에 저장
-            // if (!isPresent(getFoodName)) {
-            //     saveFood(getFoodName, getImageUrl, "치킨플러스", Category.CHICKEN);
-            // }
-        }
+    public String crawling() {
+        페리카나();
         return "redirect:/admin";
     }
 
 
-    private void BBQ(String url) throws IOException {
+    public void 페리카나() {
+        // String 오리지널 = "https://pelicana.co.kr/menu/menu_original.html";
+        // String 순살 = "https://pelicana.co.kr/menu/menu_sunsal.html";
+        // String 신선핀 = "https://pelicana.co.kr/menu/menu_sinsunpin.html";
+        // String 신선윙 = "https://pelicana.co.kr/menu/menu_sinsunwing.html";
+        // String 페리윙봉 = "https://pelicana.co.kr/menu/menu_periwing.html";
+        // String 뿌리오 = "https://pelicana.co.kr/menu/menu_bburio.html";
+        // String 맵삭치킨 = "https://pelicana.co.kr/menu/menu_mapsak.html";
+        String 로스핀 = "https://pelicana.co.kr/menu/menu_rospin.html";
+
+        Document doc;
+        try {
+            doc = Jsoup.connect(로스핀).get();
+            // 
+            Elements lists = doc.select("#contents > ul.menu_list.menu_pop > li");
+            System.out.println(">>> size: "+lists.size());
+            for (Element e : lists) {
+                // image url: #contents > ul.menu_list.menu_pop > li:nth-child(1) > a > figure > img
+                String image = e.select("a > figure > img").first().absUrl("src");
+                System.out.println(">>> image: "+image);
+                // menu name: #contents > ul.menu_list.menu_pop > li:nth-child(1) > a > figure > figcaption
+                String foodName = e.select("a > figure > figcaption").text();
+                System.out.println(">>> food: "+foodName);
+                // 만약 같은 이름의 메뉴가 없다면 DB에 저장
+                if (!isPresent(foodName)) {
+                    // 실행 전 체크할 것
+                    saveFood(foodName, image, "페리카나", Category.CHICKEN);
+                }
+            }
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+    }
+
+
+    public void 치킨플러스() {
+        // 정확한 image URL는 모달을 통하여 가져올 수 밖에 없어서 못가져올듯
+        String 치킨메뉴 = "http://www.chickenplus.co.kr/menu/default.aspx?menu=치킨메뉴";
+        // String 피자메뉴 = "http://www.chickenplus.co.kr/menu/default.aspx?menu=피자메뉴";
+        // String 떡볶이메뉴 = "http://www.chickenplus.co.kr/menu/default.aspx?menu=떡볶이메뉴";
+        Document doc;
+        try {
+            doc = Jsoup.connect(치킨메뉴).get();
+            Elements lists = doc.select("div[class=mn_area]").select("div[groupname=MenuItem]");
+            System.out.println(">>> size: "+lists.size());
+            for (Element e : lists) {
+                // menu name
+                String foodName = e.select("div.mn1_txt > p.mn1_tit.menu_s_title").text();
+                System.out.println(">>> food name: "+foodName);
+                // 만약 같은 이름의 메뉴가 없다면 DB에 저장
+                if (!isPresent(foodName)) {
+                    saveFood(foodName, "image", "치킨플러스", Category.TTEOKBOKKI);
+                }
+            }
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+    }
+
+
+    public void BBQ() throws IOException {
         String getFoodName;
         String getImageUrl;
         String urlBBQ = "https://www.bbq.co.kr/menu/menuList.asp?cidx=124&cname=1%EC%9D%B8%EB%B6%84+%EB%A9%94%EB%89%B4";
@@ -112,7 +150,7 @@ public class CrawlingController {
                 .build()
         ;
         foodService.save(food);
-        System.out.println(">>> 음식이 DB에 생성되었습니다."+food.toString());
+        System.out.println(">>> 음식이 DB에 생성되었습니다. > "+food.getName());
     }
 
 
