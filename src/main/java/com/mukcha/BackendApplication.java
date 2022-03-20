63b5beab8377dafcdabae1996d8c00a7c9085f48
Bluewind <bluewind@kakao.com>
2022-03-21 00:28:18 +0900

    포트 분할

--- src/main/java/com/mukcha/BackendApplication.java
+++ src/main/java/com/mukcha/BackendApplication.java
@@ -1,7 +1,7 @@
 package com.mukcha;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 
@@ -9,8 +9,16 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 public class BackendApplication {
 
+	public static final String APPLICATION_LOCATIONS = "spring.config.location="
+            + "classpath:application.yml,"
+            + "/app/config/mukcha/real-application.yml";
+
 	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
+		new SpringApplicationBuilder(BackendApplication.class)
+				.properties(APPLICATION_LOCATIONS)
+				.run(args)
+		;
+		// SpringApplication.run(BackendApplication.class, args);
 	}
 
 }
