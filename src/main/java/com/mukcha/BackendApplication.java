dd78daddc021b4b7d902d155a90e9a4274ced956
Bluewind <bluewind@kakao.com>
2022-03-25 21:09:33 +0900

    application Travis로 암호화

--- src/main/java/com/mukcha/BackendApplication.java
+++ src/main/java/com/mukcha/BackendApplication.java
@@ -1,24 +1,16 @@
 package com.mukcha;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 
-@EnableJpaAuditing
+@EnableJpaAuditing // JPA Auditing 활성화
 @SpringBootApplication
 public class BackendApplication {
 
-	public static final String APPLICATION_LOCATIONS = "spring.config.location="
-            + "classpath:application.yml,"
-            + "/app/config/mukcha/real-application.yml";
-
 	public static void main(String[] args) {
-		new SpringApplicationBuilder(BackendApplication.class)
-				.properties(APPLICATION_LOCATIONS)
-				.run(args)
-		;
-		// SpringApplication.run(BackendApplication.class, args);
+		SpringApplication.run(BackendApplication.class, args);
 	}
 
 }
