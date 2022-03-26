02b1488f6bee416af44b82bf1cbea7c00eff4c8b
Bluewind <bluewind@kakao.com>
2022-03-27 01:20:35 +0900

    Try fix error

--- src/main/java/com/mukcha/BackendApplication.java
+++ src/main/java/com/mukcha/BackendApplication.java
@@ -2,11 +2,12 @@ package com.mukcha;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 
 @EnableJpaAuditing // JPA Auditing 활성화
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class BackendApplication {
 
 	public static void main(String[] args) {
