22a7b06b21f85e8f30070991f16e0d2018e00965
Bluewind <bluewind@kakao.com>
2022-04-28 23:24:51 +0900

    BaseTimeEntity Test Code 추가

--- src/test/java/com/mukcha/repository/CompanyRepositoryTest.java
+++ src/test/java/com/mukcha/repository/CompanyRepositoryTest.java
@@ -2,9 +2,12 @@ package com.mukcha.repository;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.time.LocalDateTime;
+
 import com.mukcha.domain.Company;
 import com.mukcha.repository.helper.WithRepositoryTest;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -29,6 +32,26 @@ public class CompanyRepositoryTest extends WithRepositoryTest {
         // then
         assertEquals("testCompany", findCompany.getName());
         assertEquals("testCompanyLogo", findCompany.getImage());
+    }
+
+    @Test
+    void testBaseTimeEntity() {
+        // given
+        LocalDateTime now = LocalDateTime.of(2021, 4, 28, 0,0,0);
+        companyRepository.save(Company.builder()
+            .name("test")
+            .image("image")
+            .build()
+        );
+
+        // when
+        Company company = companyRepository.findAll().get(0);
+
+        // then
+        System.out.println(">>> createDate = " + company.getCreatedAt());
+        System.out.println(">>> updateDate = " + company.getUpdatedAt());
+        Assertions.assertThat(company.getCreatedAt()).isAfter(now);
+        Assertions.assertThat(company.getUpdatedAt()).isAfter(now);
     }
 
 
