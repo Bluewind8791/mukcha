3478e348dc6e6bcad8e94bbb257f402634c93f57
Bluewind <bluewind@kakao.com>
2022-04-30 00:00:32 +0900

    카테고리 별 페이지 코드 개선

--- src/main/java/com/mukcha/controller/CategoryController.java
+++ src/main/java/com/mukcha/controller/CategoryController.java
@@ -1,12 +1,9 @@
 package com.mukcha.controller;
 
-import java.util.List;
-
 import com.mukcha.config.dto.LoginUser;
 import com.mukcha.config.dto.SessionUser;
 import com.mukcha.controller.dto.UserDto;
 import com.mukcha.domain.Category;
-import com.mukcha.domain.Company;
 import com.mukcha.service.CompanyService;
 import com.mukcha.service.FoodService;
 import com.mukcha.service.UserService;
@@ -38,13 +35,12 @@ public class CategoryController {
             model.addAttribute("login_user_id", user.getUserId());
             model.addAttribute("login_user_nickname", user.getNickname());
         }
-        // 카테고리 이름으로 카테고리를 찾아서 넘겨준다
+        // 해당 카테고리
         Category thisCategory = Category.valueOf(categoryName);
         model.addAttribute("category", thisCategory);
-        // 모든 회사 리스트를 넘겨준다
-        List<Company> companyList = companyService.findAll();
-        model.addAttribute("companyList", companyList);
-        // 각 회사별 메뉴들을 카테고리 별로 분류하여 넘겨준다
+        // 모든 회사 리스트
+        model.addAttribute("companyList", companyService.findAllIntoDto());
+        // 해당 카테고리의 메뉴를 런칭한 모든 회사의 메뉴들
         model.addAttribute("foodList", 
             foodService.findAllByCategorySortByCompany(thisCategory)
         );
