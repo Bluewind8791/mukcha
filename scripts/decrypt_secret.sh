3fdfe715affc23da07e654df927d3df33a7086aa
Bluewind <bluewind@kakao.com>
2022-05-07 17:38:33 +0900

    application.yml 암호화

--- scripts/decrypt_secret.sh
+++ scripts/decrypt_secret.sh
@@ -2,4 +2,4 @@
 
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" --output ./src/main/resources/my_secret.json ./src/main/resources/my_secret.json.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" --output ./src/main/resources/application.yml ./src/main/resources/application.yml.gpg
